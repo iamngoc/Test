f7d02549c82318c456f87c2620ef697271df73cb
ngoc.nguyen <minhngocmn45@gmail.com>
2024-05-14 11:25:49 +0200

    Variable og = 10

--- Main.java
+++ Main.java
@@ -11,13 +11,14 @@ public class Main
     public static void main (String args[])
     {
         int summe = 0;
-        for (int i = 1; i <=10; i++)
+        int og = 10;
+        for (int i = 1; i <=og; i++)
         {
             summe = summe +1;
         }
         
         int mul = 1;
-        for (int i = 2; i <= 10; i++)
+        for (int i = 2; i <= og; i++)
         {
             mul *= i;
         }
