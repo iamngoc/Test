e11495ce5d142e7d0bed0dbb260b5c0e2862fb85
ngoc.nguyen <minhngocmn45@gmail.com>
2024-05-14 10:47:38 +0200

    Main.java implementiert

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Klasse Main.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+
+public class Main
+{
+    public static void main (String args[])
+    {
+        
+    }
+}
