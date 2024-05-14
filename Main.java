f8e9e5a21ff40f56ff7c1e785dd036dadc186dec
ngoc.nguyen <minhngocmn45@gmail.com>
2024-05-14 11:27:14 +0200

    Variable obereGrenze = 20

--- Main.java
+++ Main.java
@@ -10,14 +10,15 @@ public class Main
 {
     public static void main (String args[])
     {
+        int obereGrenze = 20;
         int summe = 0;
-        for (int i = 1; i <=10; i++)
+        for (int i = 1; i <=obereGrenze; i++)
         {
             summe = summe +1;
         }
         
         int mul = 1;
-        for (int i = 2; i <= 10; i++)
+        for (int i = 2; i <= obereGrenze; i++)
         {
             mul *= i;
         }
