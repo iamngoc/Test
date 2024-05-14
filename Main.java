a7e26ce871c5b997170faeeb0069749ce02be32e
iamngoc <168901457+iamngoc@users.noreply.github.com>
2024-05-14 12:17:59 +0200

    ObereGrenze = 75

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main
 {
     public static void main (String args[])
     {
-        int obereGrenze = 20;
+        int obereGrenze = 75;
         int summe = 0;
 
         for (int i = 1; i <=obereGrenze; i++)
